1ec464c965428eb281b2795a9cd9e7bc48a19a9a
Giugurtah <c.buoncompagni.98@gmail.com>
2021-10-19 17:42:02 +0200

    Utilizzo faker

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Post;
+use Faker\Generator as Faker;
 
 class PostSeeder extends Seeder
 {
@@ -10,27 +11,15 @@ class PostSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        $posts = [
-            [
-                'title' => 'Post 3',
-                'content' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta dolorum exercitationem nulla itaque quis? Provident illo nisi consequatur alias laboriosam aut earum minus. Ipsum, quisquam non quos consequuntur cum dolorem voluptatibus architecto? Illum sint totam temporibus officiis rem voluptate vitae aspernatur vel veniam debitis fuga minima pariatur ab quo voluptatum facere odit esse, expedita atque repellat iste, itaque inventore officia consequatur. Eveniet sint perferendis ex laborum, quidem tempore. In ex aliquid consectetur explicabo maiores hic amet aut. Dolorum quibusdam consequuntur iste amet voluptates accusamus a quo delectus voluptas ut inventore quae quidem eveniet hic exercitationem perferendis, magni rem. Nobis fugiat cumque commodi ratione unde non nemo libero, eos exercitationem accusantium perferendis sequi voluptatum voluptatem fugit iusto in similique! Molestias, laborum minima! Facilis perspiciatis beatae, asperiores maxime perferendis possimus quasi error unde ratione maiores facere architecto magni aspernatur optio ullam laborum recusandae voluptates illo, dignissimos nisi? Eius dolorem tenetur nostrum asperiores',
-            ],
-            [
-                'title' => 'Post 4',
-                'content' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta dolorum exercitationem nulla itaque quis? Provident illo nisi consequatur alias laboriosam aut earum minus. Ipsum, quisquam non quos consequuntur cum dolorem voluptatibus architecto? Illum sint totam temporibus officiis rem voluptate vitae aspernatur vel veniam debitis fuga minima pariatur ab quo voluptatum facere odit esse, expedita atque repellat iste, itaque inventore officia consequatur. Eveniet sint perferendis ex laborum, quidem tempore. In ex aliquid consectetur explicabo maiores hic amet aut. Dolorum quibusdam consequuntur iste amet voluptates accusamus a quo delectus voluptas ut inventore quae quidem eveniet hic exercitationem perferendis, magni rem. Nobis fugiat cumque commodi ratione unde non nemo libero, eos exercitationem accusantium perferendis sequi voluptatum voluptatem fugit iusto in similique! Molestias, laborum minima! Facilis perspiciatis beatae, asperiores maxime perferendis possimus quasi error unde ratione maiores facere architecto magni aspernatur optio ullam laborum recusandae voluptates illo, dignissimos nisi? Eius dolorem tenetur nostrum asperiores',
-            ],
-            [
-                'title' => 'Post 5',
-                'content' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta dolorum exercitationem nulla itaque quis? Provident illo nisi consequatur alias laboriosam aut earum minus. Ipsum, quisquam non quos consequuntur cum dolorem voluptatibus architecto? Illum sint totam temporibus officiis rem voluptate vitae aspernatur vel veniam debitis fuga minima pariatur ab quo voluptatum facere odit esse, expedita atque repellat iste, itaque inventore officia consequatur. Eveniet sint perferendis ex laborum, quidem tempore. In ex aliquid consectetur explicabo maiores hic amet aut. Dolorum quibusdam consequuntur iste amet voluptates accusamus a quo delectus voluptas ut inventore quae quidem eveniet hic exercitationem perferendis, magni rem. Nobis fugiat cumque commodi ratione unde non nemo libero, eos exercitationem accusantium perferendis sequi voluptatum voluptatem fugit iusto in similique! Molestias, laborum minima! Facilis perspiciatis beatae, asperiores maxime perferendis possimus quasi error unde ratione maiores facere architecto magni aspernatur optio ullam laborum recusandae voluptates illo, dignissimos nisi? Eius dolorem tenetur nostrum asperiores',
-            ],
-        ];
+        for($i = 0; $i < 25; $i++) {
+            $post = new Post;
+            $post->title = $faker->words(5, true);
+            $post->content = $faker->paragraphs(3, true);
+            $post->image = $faker->imageUrl(300,300,'animals');
 
-        foreach($posts as $post) {
-            $post_aux = new Post();
-            $post_aux->fill($post);
-            $post_aux->save();
+            $post->save();
         }
     }
 }
